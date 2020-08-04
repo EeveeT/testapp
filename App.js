941b5c4b7366ef5e729f6aea50e0088365a3166f
evytang <32822904+EeveeT@users.noreply.github.com>
2020-08-04 22:05:29 +0100

    Finished code from tutorial

--- App.js
+++ App.js
@@ -4,10 +4,26 @@ import {
   useLoadScript,
   Marker,
   InfoWindow,
+  useGoogleMap,
 } from "@react-google-maps/api";
-//import { formatRelative } from "date-fns";
 
-//import "@reach/combobox/styles.css";
+import usePlacesAutoComplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import { formatRelative } from "date-fns";
+
+import "@reach/combobox/styles.css";
+
+import mapStyles from "./mapStyles";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -18,6 +34,11 @@ const center = {
   lat: 51.4545,
   lng: -2.5879,
 };
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
 
 export default function App() {
   const { isLoaded, loadError } = useLoadScript({
@@ -25,16 +46,172 @@ export default function App() {
     libraries,
   });
 
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  /* For a user to set a marker on the map which
+has long/lat and time stamped from when user
+clicks on map */
+  // Use callback allows you to create a function that will always retain the same value
+  // unless the depths change past(?)(?)))
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(15);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
 
   return (
     <div>
+      <h1>
+        Historic Sites of Interest{" "}
+        <span role="img< aria-label = " site of interest>
+          📌
+        </span>
+      </h1>
+
+      <Search panTo={panTo} />
+      <Locate panTo={panTo} />
+
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        zoom={14}
+        zoom={15}
         center={center}
-      ></GoogleMap>
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              // to personalise the marker stamp, used own image
+              url: "/evy.png",
+              // to scale the image to the size I wanted on the map
+              scaledSize: new window.google.maps.Size(30, 50),
+              // origin and anchor ensure that a marker will be places exactly
+              // where I click
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            // setting selected to null on close clicks enables us to close and re-click on
+            // the info window for each marker - toggles on and off
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Evy was here</h2>
+              <p>
+                A wild Evy appeared {formatRelative(selected.time, new Date())}
+              </p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
   );
 }
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+            console.log(position);
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="evyWinter.png" alt="evy" />
+    </button>
+  );
+}
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutoComplete({
+    requestOptions: {
+      location: { lat: () => 51.4545, lng: () => -2.5879 },
+      radius: 200000,
+    },
+  });
+
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            console.log(lat, lng);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log("error!");
+          }
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter an address"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
+
+//Following tutorial from: https://www.youtube.com/watch?v=WZcxJGmLbSo
+// Good recap for first half of tutorial is at 26 minutes in.
